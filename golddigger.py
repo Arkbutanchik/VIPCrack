ec4424e0cb7b128928ebc3493c204a0fa3ea59c8
arkbutan <arkbutan@arkbutan.arkbutan>
2025-04-27 13:52:27 +0300

    1st parse iter

--- golddigger.py
+++ golddigger.py
@@ -1,1 +1,15 @@
-# god dayum
+from fabric import Connection
+
+HOST = '192.168.1.9'
+USER = 'arkbutan'
+PASSWD = 'arkbutan'
+
+def run_ssh_command(command):
+
+    conn = Connection(host=HOST, user=USER, connect_kwargs={'password': PASSWD})
+
+    return conn.run(command, hide=True).stdout.split('\n')
+
+
+for i in run_ssh_command('ls'):
+    print(run_ssh_command(f'cd {i} && ls'))
