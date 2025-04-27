9e4eaf6e8916c820150ec41a2c1f5b0ff1f33841
arkbutan <arkbutan@arkbutan.arkbutan>
2025-04-27 14:03:38 +0300

    added command line args

--- golddigger.py
+++ golddigger.py
@@ -1,15 +1,27 @@
 from fabric import Connection
+import argparse
 
-HOST = '192.168.1.9'
-USER = 'arkbutan'
-PASSWD = 'arkbutan'
+
+parser = argparse.ArgumentParser()
+    
+parser.add_argument('-H', '--host', dest='HOST', required=True, help='Target host IP address')
+parser.add_argument('-u', '--user', dest='USER', required=True, help='Username for authentication')
+parser.add_argument('-p', '--pass', dest='PASSWD', required=True, help='Password for authentication')
+    
+args = parser.parse_args()
+    
+HOST = args.HOST
+USER = args.USER
+PASSWD = args.PASSWD
+    
+print(f"Connecting to {HOST} with user {USER} and password {PASSWD}")
+    
 
 def run_ssh_command(command):
 
     conn = Connection(host=HOST, user=USER, connect_kwargs={'password': PASSWD})
 
     return conn.run(command, hide=True).stdout.split('\n')
-
 
 for i in run_ssh_command('ls'):
     print(run_ssh_command(f'cd {i} && ls'))
