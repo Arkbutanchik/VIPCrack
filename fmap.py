9b0035530805ed6fce27ec27ca1d000fa3f7c8ad
arkbutan <arkbutan@arkbutan.arkbutan>
2025-04-28 20:34:42 +0300

    golddigger 1st ver

---
+++ fmap.py
@@ -0,0 +1,16 @@
+import argparse
+import datetime
+
+# parser = argparse.ArgumentParser()
+    
+# parser.add_argument('-H', '--host', dest='HOST', required=True, help='Target host IP address')
+# parser.add_argument('-u', '--user', dest='USER', required=True, help='Username for authentication')
+# parser.add_argument('-p', '--pass', dest='PASSWD', required=True, help='Password for authentication')
+    
+# args = parser.parse_args()
+    
+# HOST = args.HOST
+# USER = args.USER
+# PASSWD = args.PASSWD
+
+print(f'Starting Fmap at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}\n')
